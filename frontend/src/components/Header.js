c04e45babf75e045663541e3d5b4e9ec28d3b70b
Shubhamdutta2000 <dondutta2000@gmail.com>
2020-12-11 23:52:14 +0530

    add carousal to electronics products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,51 @@
-import React from 'react'
-import { Navbar, Nav, Form, FormControl, Button, Container } from "react-bootstrap"
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
 
 const Header = () => {
-     return (
-          <header>
-               <Navbar className="navbar py-2" sticky="top" expand="lg" collapseOnSelect>
-                    <Container>
-                         <Navbar.Brand className="text-white" style={{fontSize: '1.8rem'}} href="/">eShop</Navbar.Brand>
-                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                         <Navbar.Collapse id="basic-navbar-nav">
-                              <Nav className="ml-auto">
-                                   <Nav.Link className="nav_link text-white pr-4" href="/cart"><i className="fa fa-shopping-cart pr-1" aria-hidden="true"></i> CART</Nav.Link>
-                                   <Nav.Link className="nav_link text-white"  href="/signup"><i className="fa fa-user-plus pr-1" aria-hidden="true"></i> SIGN IN</Nav.Link>
-                              </Nav>
-                              <Form className="pl-5" inline>
-                                   <FormControl className="search_box" type="text" placeholder="Search" className="mr-sm-2" />
-                                   <Button variant="outline-secondary">Search</Button>
-                              </Form>
-                         </Navbar.Collapse>
-                    </Container>
-               </Navbar>
-          </header>
-     )
-}
+  return (
+    <header>
+      <Navbar className="navbar py-2" sticky="top" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand
+            className="text-white"
+            style={{ fontSize: "1.8rem" }}
+            href="/"
+          >
+            eShop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link className="nav_link text-white pr-4" href="/cart">
+                <i className="fa fa-shopping-cart pr-1" aria-hidden="true"></i>{" "}
+                CART
+              </Nav.Link>
+              <Nav.Link className="nav_link text-white" href="/signup">
+                <i className="fa fa-user-plus pr-1" aria-hidden="true"></i> SIGN
+                IN
+              </Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                className="search_box"
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-secondary">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/screen/Home.js
+++ frontend/src/screen/Home.js
@@ -1,22 +1,50 @@
-import React from 'react'
-import {Row, Col} from "react-bootstrap";
-import Product from "../components/Products"
-import {electronics, mens_accessories, womens_accessories, home_appliances} from "../products";
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Products";
+
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
+import {
+  electronics,
+  mens_accessories,
+  womens_accessories,
+  home_appliances,
+} from "../products";
 
 export default function Home() {
-     return (
-          <>
-          <h1> Electronics Accessories </h1>
-          <Row>
-               {electronics.map((electronic) => (
-
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                         <Product product={electronic} />
-                    </Col>
-
-               ))}
-          </Row>
-               
-          </>
-     )
+  const options = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  };
+  return (
+    <>
+      <h1> Electronics Accessories </h1>
+      <Row>
+        <OwlCarousel className="owl-theme" {...options}>
+          {electronics.map((electronic) => (
+            <Col>
+              <Product product={electronic} />
+            </Col>
+          ))}
+        </OwlCarousel>
+      </Row>
+    </>
+  );
 }
