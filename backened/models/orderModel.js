b98931b657b0165121d437887916e3ffafaf0a02
Shubhamdutta2000 <dondutta2000@gmail.com>
2020-12-16 00:29:14 +0530

    BACKENED: add models of USER, PRODUCT, ORDER

---
+++ backened/models/orderModel.js
@@ -0,0 +1,90 @@
+import mongoose from "mongoose";
+
+/////////////    Order Schema     ///////////////
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: String, required: true },
+        product: {
+          type: mongoose.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      updateTime: { type: String },
+      emailAddress: { type: String },
+    },
+
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    paidAt: {
+      type: Date,
+    },
+
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
---
+++ backened/models/productsModel.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+
+//////////// reviews Schema of customer   ///////////
+
+const reviewsSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+/////////////    Product Schema     ///////////////
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    reviews: [reviewsSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
