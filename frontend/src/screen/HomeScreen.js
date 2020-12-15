e648ebf0c97a6a78e3001bd09a644b4a75e71e5e
Shubhamdutta2000 <dondutta2000@gmail.com>
2020-12-15 21:01:32 +0530

    fix issue of render OwlCatousal before children of State (by adding key prop to carousal)

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -39,74 +39,112 @@ export default function Home() {
 
   const [products, setProducts] = useState([]);
 
+  const [category, setCategory] = useState({
+    electronics: [],
+    home_appliances: [],
+    mens_accessories: [],
+    womens_accessories: [],
+  });
+
   useEffect(() => {
     const fetchProducts = async () => {
       const { data } = await axios.get("/products");
 
       setProducts(data);
-      console.log(data.filter((p) => p.category == "electronics"));
+
+      // filter all products by category
+      setCategory({
+        electronics: data.filter((p) => p.category == "electronics"),
+        home_appliances: data.filter((p) => p.category == "home_appliances"),
+        mens_accessories: data.filter((p) => p.category == "mens_accessories"),
+        womens_accessories: data.filter(
+          (p) => p.category == "womens_accessories"
+        ),
+      });
     };
 
     fetchProducts();
   }, []);
+
+  // console.log(category);
+
   return (
     <>
       {/*//////////////////////////       CAROUSAL      /////////////////////////////////////// */}
 
+      {/* /////////////////    BUG: Render carusal before children ////////// */}
+      {/*//////////////////    SOLUTION: ADD key to OwlCarousal  */}
+
       {/* ELECTRONICS */}
       <h1 className="pt-5"> Electronics Accessories </h1>
       <Row>
-        <OwlCarousel className="owl-theme" {...options}>
-          {products
-            .filter((p) => p.category === "electronics")
-            .map((electronic, index) => (
+        {category && (
+          <OwlCarousel
+            key={category.electronics.length}
+            className="owl-theme"
+            {...options}
+          >
+            {category.electronics.map((electronic, index) => (
               <Col key={index}>
                 <Product product={electronic} />
               </Col>
             ))}
-        </OwlCarousel>
+          </OwlCarousel>
+        )}
       </Row>
 
       {/* HOME APPLIANCES */}
       <h1 className="pt-5">Home Appliances</h1>
       <Row>
-        <OwlCarousel className="owl-theme" {...options}>
-          {products
-            .filter((p) => p.category == "home_appliances")
-            .map((home_appliance, index) => (
+        {category && (
+          <OwlCarousel
+            key={category.home_appliances.length}
+            className="owl-theme"
+            {...options}
+          >
+            {category.home_appliances.map((home_appliance, index) => (
               <Col key={index}>
                 <Product product={home_appliance} />
               </Col>
             ))}
-        </OwlCarousel>
+          </OwlCarousel>
+        )}
       </Row>
 
       {/* MENS'S ACCESSORIES */}
       <h1 className="pt-5">Men's Accessories</h1>
       <Row>
-        <OwlCarousel className="owl-theme" {...options}>
-          {products
-            .filter((p) => p.category == "mens_accessories")
-            .map((mens_accessory, index) => (
+        {category && (
+          <OwlCarousel
+            key={category.mens_accessories.length}
+            className="owl-theme"
+            {...options}
+          >
+            {category.mens_accessories.map((mens_accessory, index) => (
               <Col key={index}>
                 <Product product={mens_accessory} />
               </Col>
             ))}
-        </OwlCarousel>
+          </OwlCarousel>
+        )}
       </Row>
 
       {/* WOMEN'S ACCESSORIES */}
       <h1 className="pt-5">Women's Accessories</h1>
       <Row>
-        <OwlCarousel className="owl-theme" {...options}>
-          {products
-            .filter((p) => p.category == "womens_accessories")
-            .map((womens_accessory, index) => (
+        {category && (
+          <OwlCarousel
+            key={category.womens_accessories.length}
+            className="owl-theme"
+            {...options}
+          >
+            {category.womens_accessories.map((womens_accessory, index) => (
               <Col key={index}>
                 <Product product={womens_accessory} />
               </Col>
             ))}
-        </OwlCarousel>
+          </OwlCarousel>
+        )}
       </Row>
 
       {/*//////////////////////////       CAROUSAL ENDS      /////////////////////////////////////// */}
@@ -114,9 +152,9 @@ export default function Home() {
       {/* ALL PRODUCTS */}
       <h1 className="pt-5">Latest Products</h1>
       <Row>
-        {products.map((home_appliance, index) => (
+        {products.map((product, index) => (
           <Col md={3} key={index}>
-            <Product product={home_appliance} />
+            <Product product={product} />
           </Col>
         ))}
       </Row>
