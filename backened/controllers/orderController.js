0d39fa3c43f5ed788ee1a05d64f4591c466a11c1
Shubhamdutta2000 <dondutta2000@gmail.com>
2020-12-31 10:32:01 +0530

    BACKENED: endpoint of Update to paid

--- backened/controllers/orderController.js
+++ backened/controllers/orderController.js
@@ -39,7 +39,7 @@ export const addOrderItems = asyncHandler(async (req, res) => {
 
 //  @purpose: get order by id
 //  @access:  Private
-//  @route:   POST /order/:id
+//  @route:   GET /order/:id
 
 export const getOrderById = asyncHandler(async (req, res) => {
   const order = await OrderModel.findById(req.params.id).populate(
@@ -49,6 +49,30 @@ export const getOrderById = asyncHandler(async (req, res) => {
 
   if (order) {
     res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not Found");
+  }
+});
+
+//  @purpose: UPDATE order model to paid
+//  @access:  Private
+//  @route:   UPDATE /order/:id/payment
+
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await OrderModel.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      updateTime: req.body.update_time,
+      emailAddress: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
   } else {
     res.status(404);
     throw new Error("Order not Found");
--- backened/routes/OrderRoutes.js
+++ backened/routes/OrderRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import Protect from "../middleware/authMiddleware.js";
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 
 const router = express.Router();
 
 router.route("/").post(Protect, addOrderItems);
 router.route("/:id").get(Protect, getOrderById);
+router.route("/:id/payment").put(Protect, updateOrderToPaid);
 
 export default router;
