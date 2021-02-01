60e62eb38a998b1c7c3f1a310a99734ab25c439f
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-01 17:54:16 +0530

    design carousal and added to site

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -2,16 +2,23 @@ import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Products";
 
+// Owl Carousal
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
+// Redux
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../redux/actions/productListAction";
 
+// Components
 import Loader from "../components/Loader";
 import ErrMess from "../components/ErrMessage";
 
+// Carousal Data
+import { carousalData } from "../utils/carousalData";
+
+// Styling
 import "../styles/Screen/HomeScreen.css";
 
 export default function Home() {
@@ -29,7 +36,7 @@ export default function Home() {
       0: {
         items: 0,
       },
-      480: {
+      320: {
         items: 1,
       },
       768: {
@@ -41,6 +48,13 @@ export default function Home() {
     },
   };
 
+  const options2 = {
+    loop: false,
+    margin: 100,
+    nav: true,
+    items: 1,
+  };
+
   //////////////////     fetching datas of productList from redux state   ////////////////////////
 
   const dispatch = useDispatch();
@@ -49,141 +63,170 @@ export default function Home() {
 
   useEffect(() => {
     dispatch(listProducts());
+    console.log(carousalData);
   }, [dispatch]);
 
   return (
     <>
       {/*//////////////////////////       CAROUSAL      /////////////////////////////////////// */}
 
+      <Row>
+        <OwlCarousel key={products.length} className="owl-theme" {...options2}>
+          {carousalData.map((data, index) => (
+            <Col key={index}>
+              <div
+                className="carousal"
+                style={{
+                  background: `url(${data.image})`,
+                  backgroundPosition: "0rem",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                <h1 className="carousal_heading">{data.heading}</h1>
+                <h2 className="carousal_para">{data.para}</h2>
+                <button className="carousal_button">SHOP NOW</button>
+              </div>
+              {/* <img
+                className="carousal_img"
+                src={data.image}
+                alt="electronics"
+              /> */}
+            </Col>
+          ))}
+        </OwlCarousel>
+      </Row>
       {/* /////////////////    BUG: Render carusal before children ////////// */}
       {/*//////////////////    SOLUTION: ADD key to OwlCarousal  */}
 
-      {/* ELECTRONICS */}
-      <h1 className="pt-5"> Electronics Accessories </h1>
+      <div className="mx-5 px-4">
+        {/* ELECTRONICS */}
+        <h1 className="pt-5"> Electronics Accessories </h1>
 
-      <Row>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <ErrMess varient="#FC308B">{error}</ErrMess>
-        ) : (
-          products && (
-            <OwlCarousel
-              key={products.length}
-              className="owl-theme"
-              {...options}
-            >
-              {products
-                .filter((p) => p.category == "electronics")
-                .map((electronic, index) => (
-                  <Col key={index}>
-                    <Product product={electronic} />
-                  </Col>
-                ))}
-            </OwlCarousel>
-          )
-        )}
-      </Row>
+        <Row>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <ErrMess varient="#FC308B">{error}</ErrMess>
+          ) : (
+            products && (
+              <OwlCarousel
+                key={products.length}
+                className="owl-theme"
+                {...options}
+              >
+                {products
+                  .filter((p) => p.category == "electronics")
+                  .map((electronic, index) => (
+                    <Col key={index}>
+                      <Product product={electronic} />
+                    </Col>
+                  ))}
+              </OwlCarousel>
+            )
+          )}
+        </Row>
 
-      {/* HOME APPLIANCES */}
-      <h1 className="pt-5">Home Appliances</h1>
+        {/* HOME APPLIANCES */}
+        <h1 className="pt-5">Home Appliances</h1>
 
-      <Row>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <ErrMess varient="#FC308B">{error}</ErrMess>
-        ) : (
-          products && (
-            <OwlCarousel
-              key={products.length}
-              className="owl-theme"
-              {...options}
-            >
-              {products
-                .filter((p) => p.category == "home_appliances")
-                .map((home_appliance, index) => (
-                  <Col key={index}>
-                    <Product product={home_appliance} />
-                  </Col>
-                ))}
-            </OwlCarousel>
-          )
-        )}
-      </Row>
+        <Row>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <ErrMess varient="#FC308B">{error}</ErrMess>
+          ) : (
+            products && (
+              <OwlCarousel
+                key={products.length}
+                className="owl-theme"
+                {...options}
+              >
+                {products
+                  .filter((p) => p.category == "home_appliances")
+                  .map((home_appliance, index) => (
+                    <Col key={index}>
+                      <Product product={home_appliance} />
+                    </Col>
+                  ))}
+              </OwlCarousel>
+            )
+          )}
+        </Row>
 
-      {/* MENS'S ACCESSORIES */}
-      <h1 className="pt-5">Men's Accessories</h1>
+        {/* MENS'S ACCESSORIES */}
+        <h1 className="pt-5">Men's Accessories</h1>
 
-      <Row>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <ErrMess varient="#FC308B">{error}</ErrMess>
-        ) : (
-          products && (
-            <OwlCarousel
-              key={products.length}
-              className="owl-theme"
-              {...options}
-            >
-              {products
-                .filter((p) => p.category == "mens_accessories")
-                .map((mens_accessory, index) => (
-                  <Col key={index}>
-                    <Product product={mens_accessory} />
-                  </Col>
-                ))}
-            </OwlCarousel>
-          )
-        )}
-      </Row>
+        <Row>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <ErrMess varient="#FC308B">{error}</ErrMess>
+          ) : (
+            products && (
+              <OwlCarousel
+                key={products.length}
+                className="owl-theme"
+                {...options}
+              >
+                {products
+                  .filter((p) => p.category == "mens_accessories")
+                  .map((mens_accessory, index) => (
+                    <Col key={index}>
+                      <Product product={mens_accessory} />
+                    </Col>
+                  ))}
+              </OwlCarousel>
+            )
+          )}
+        </Row>
 
-      {/* WOMEN'S ACCESSORIES */}
-      <h1 className="pt-5">Women's Accessories</h1>
+        {/* WOMEN'S ACCESSORIES */}
+        <h1 className="pt-5">Women's Accessories</h1>
 
-      <Row>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <ErrMess varient="#FC308B">{error}</ErrMess>
-        ) : (
-          products && (
-            <OwlCarousel
-              key={products.length}
-              className="owl-theme"
-              {...options}
-            >
-              {products
-                .filter((p) => p.category == "womens_accessories")
-                .map((womens_accessory, index) => (
-                  <Col key={index}>
-                    <Product product={womens_accessory} />
-                  </Col>
-                ))}
-            </OwlCarousel>
-          )
-        )}
-      </Row>
+        <Row>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <ErrMess varient="#FC308B">{error}</ErrMess>
+          ) : (
+            products && (
+              <OwlCarousel
+                key={products.length}
+                className="owl-theme"
+                {...options}
+              >
+                {products
+                  .filter((p) => p.category == "womens_accessories")
+                  .map((womens_accessory, index) => (
+                    <Col key={index}>
+                      <Product product={womens_accessory} />
+                    </Col>
+                  ))}
+              </OwlCarousel>
+            )
+          )}
+        </Row>
 
-      {/*//////////////////////////       CAROUSAL ENDS      /////////////////////////////////////// */}
+        {/*//////////////////////////       CAROUSAL ENDS      /////////////////////////////////////// */}
 
-      {/* ALL PRODUCTS */}
-      <h1 className="pt-5">Latest Products</h1>
+        {/* ALL PRODUCTS */}
+        <h1 className="pt-5">Latest Products</h1>
 
-      <Row>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <ErrMess varient="#FC308B">{error}</ErrMess>
-        ) : (
-          products.map((product, index) => (
-            <Col md={4} lg={3} key={index}>
-              <Product product={product} />
-            </Col>
-          ))
-        )}
-      </Row>
+        <Row>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <ErrMess varient="#FC308B">{error}</ErrMess>
+          ) : (
+            products.map((product, index) => (
+              <Col md={4} lg={3} key={index}>
+                <Product product={product} />
+              </Col>
+            ))
+          )}
+        </Row>
+      </div>
     </>
   );
 }
---
+++ frontend/src/utils/carousalData.js
@@ -0,0 +1,26 @@
+export const carousalData = [
+  {
+    image: "/assets/images/carousal/Landing_electronics.png",
+    heading: "Electronics Accessories",
+    para:
+      "Lorem Ipsum is simply dummy text of the print and type setting industry.",
+  },
+  {
+    image: "/assets/images/carousal/Landing_home.png",
+    heading: "Home Appliances",
+    para:
+      "Lorem Ipsum is simply dummy text of the print and type setting industry.",
+  },
+  {
+    image: "/assets/images/carousal/Landing_mens.png",
+    heading: "Mens Accessories",
+    para:
+      "Lorem Ipsum is simply dummy text of the print and type setting industry.",
+  },
+  {
+    image: "/assets/images/carousal/Landing_womens.png",
+    heading: "Womens Accessories",
+    para:
+      "Lorem Ipsum is simply dummy text of the print and type setting industry.",
+  },
+];
