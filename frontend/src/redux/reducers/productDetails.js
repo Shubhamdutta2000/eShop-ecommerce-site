a54206ec77ccb59d994abc52e06626275a60158b
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-02 19:34:44 +0530

    add redux reducer action for productReview in frontend

--- frontend/src/redux/reducers/productDetails.js
+++ frontend/src/redux/reducers/productDetails.js
@@ -2,6 +2,10 @@ import {
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAILED,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_FAILED,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_RESET,
 } from "../actionTypes/productDetailsConstants";
 
 export const productDetailsReducer = (
@@ -16,6 +20,21 @@ export const productDetailsReducer = (
 
     case PRODUCT_DETAILS_FAILED:
       return { loading: false, product: {}, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productCreateReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true, error: null };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { loading: false, success: true };
+    case PRODUCT_CREATE_REVIEW_FAILED:
+      return { loading: false, error: action.payload };
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {};
     default:
       return state;
   }
