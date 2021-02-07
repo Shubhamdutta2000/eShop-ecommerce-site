10a285af0f102ee0ab3f8d75cdaed9f88f862e30
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-07 14:14:47 +0530

    writing frontend for stripe (only calling post req from backend)

--- frontend/src/components/StripeCheckout.js
+++ frontend/src/components/StripeCheckout.js
@@ -1,9 +1,30 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import StripeCheckoutButton from "react-stripe-checkout";
+import axios from "axios";
+
+// REDUX
+import { useSelector } from "react-redux";
 
 export const StripeCheckout = () => {
-  const makePayment = (token) => {};
+  // User Login Credentials
+  const login = useSelector((state) => state.userLogin);
+  const { userInfo } = login;
+
+  // make payment through stripe by post request data to backend
+  const makePayment = async (token) => {
+    try {
+      const { data } = await axios.post("/payment/stripe", token, {
+        "Content-Type": "application/json",
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <StripeCheckoutButton
