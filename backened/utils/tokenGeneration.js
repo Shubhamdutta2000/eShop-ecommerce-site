39a31b3197ee1a1a1153b417b8e333902d3ae833
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-03-10 00:41:01 +0530

    increase jwt expiry time

--- backened/utils/tokenGeneration.js
+++ backened/utils/tokenGeneration.js
@@ -2,14 +2,8 @@ import jwt from "jsonwebtoken";
 
 const generateAccessToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_ACCESS_TOKEN_KEY, {
-    expiresIn: "30s",
+    expiresIn: "10d",
   });
 };
 
-const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_REFRESH_TOKEN_KEY, {
-    expiresIn: "1y",
-  });
-};
-
-export { generateAccessToken, generateRefreshToken };
+export { generateAccessToken };
