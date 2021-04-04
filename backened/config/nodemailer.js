8f4ba5ba34953cb2bcb79fbfc144f64c2476db2f
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-04-04 19:07:25 +0530

    add nodemailer config file

---
+++ backened/config/nodemailer.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+import ejs from "ejs";
+import path from "path";
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "shubhamdutta1511@gmail.com", // generated ethereal user
+    pass: "shubham#2000", // generated ethereal password
+  },
+});
+
+let renderTemplate = (data, relativePath) => {
+  let mainHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("Error in rendering!!");
+        return;
+      }
+      mainHTML = template;
+    }
+  );
+  return mainHTML;
+};
+
+export { transporter, renderTemplate };
